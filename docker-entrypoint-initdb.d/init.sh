b674728df542f4e67a1473e402506750f1daa30c
Gary Sieling <gsieling@us.imshealth.com>
2018-08-13 19:56:36 -0400

    create db

--- docker-entrypoint-initdb.d/init.sh
+++ docker-entrypoint-initdb.d/init.sh
@@ -8,11 +8,13 @@ pwd
 
 find .
 
+psql -c "create database imdb;"
+
 set PATH=.
 /home/postgres/miniconda3/bin/python3 vec2database.py config/vecs_config.json
 /home/postgres/miniconda3/bin/python3 vec2database.py config/vecs_norm_config.json
 /home/postgres/miniconda3/bin/python3 pq_index.py config/pq_config.json
 /home/postgres/miniconda3/bin/python3 ivfadc.py config/ivfadc_config.json
 
-psql "create extension freddy;"
-psql "SELECT init('google_vecs', 'google_vecs_norm', 'pq_quantization', 'pq_codebook', 'fine_quantization', 'coarse_quantization', 'residual_codebook');"
+psql -c "create extension freddy;"
+psql -c "SELECT init('google_vecs', 'google_vecs_norm', 'pq_quantization', 'pq_codebook', 'fine_quantization', 'coarse_quantization', 'residual_codebook');"
