e0d7a7b912251951a02f6da2f6eaa9dbbd9a15eb
gary <gary@garysieling.com>
2018-08-26 13:15:46 -0400

    Add an example

--- examples.sql
+++ examples.sql
@@ -23,3 +23,41 @@ where term in (
     'postgresql'
 )
 group by term, location;
+
+-- add up all the vectors for terms
+select
+   agg_vec_plus_bytea(vector),
+   term
+from (
+    select
+        term,
+        google_vecs.vector,
+        google_vecs.id
+    from (
+        select
+            term
+        from (
+            select
+                lower(json_array_elements_text(words)) term,
+                location
+            /*query,
+                 index,
+                 title,
+                 url,
+                 company,
+                 location,
+                 description*/
+            from jobs
+        ) words
+        /*where term in (
+            'javascript',
+            'css',
+            'scala',
+            'postgresql'
+        )*/
+     ) terms
+     left join google_vecs on google_vecs.word = terms.term
+ ) vectors
+ group by term
+-- what rare skills do I have?
+
